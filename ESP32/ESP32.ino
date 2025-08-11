5c6e40037b671486d7aeb6d717ba39d4a6400423
Vladyslav Neichev <vlad27neychev95@gmail.com>
2025-08-12 00:46:56 +0300

    removed redundant defines

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -6,26 +6,6 @@
 
 #define CAMERA_MODEL_AI_THINKER
 
-// #if defined(CAMERA_MODEL_WROVER_KIT)
-// #define PWDN_GPIO_NUM -1
-// #define RESET_GPIO_NUM -1
-// #define XCLK_GPIO_NUM 21
-// #define SIOD_GPIO_NUM 26
-// #define SIOC_GPIO_NUM 27
-
-// #define Y9_GPIO_NUM 35
-// #define Y8_GPIO_NUM 34
-// #define Y7_GPIO_NUM 39
-// #define Y6_GPIO_NUM 36
-// #define Y5_GPIO_NUM 19
-// #define Y4_GPIO_NUM 18
-// #define Y3_GPIO_NUM 5
-// #define Y2_GPIO_NUM 4
-// #define VSYNC_GPIO_NUM 25
-// #define HREF_GPIO_NUM 23
-// #define PCLK_GPIO_NUM 22
-
-// #elif defined(CAMERA_MODEL_AI_THINKER)
 #define PWDN_GPIO_NUM 32
 #define RESET_GPIO_NUM -1
 #define XCLK_GPIO_NUM 0
@@ -43,10 +23,6 @@
 #define VSYNC_GPIO_NUM 25
 #define HREF_GPIO_NUM 23
 #define PCLK_GPIO_NUM 22
-
-// #else
-// #error "Camera model not selected"
-// #endif
 
 // GPIO Setting
 extern int gpLed = 4; // Light
@@ -86,21 +62,21 @@ void setup()
   if (psramFound())
   {
     config.frame_size = FRAMESIZE_HVGA;
-    FRAMESIZE_96X96,              // 96x96
-        FRAMESIZE_QQVGA,          // 160x120
-        FRAMESIZE_QCIF,           // 176x144
-        FRAMESIZE_HQVGA,          // 240x176
-        FRAMESIZE_240X240,        // 240x240
-        FRAMESIZE_QVGA,           // 320x240
-        FRAMESIZE_CIF,            // 400x296
-        FRAMESIZE_HVGA,           // 480x320
-        FRAMESIZE_VGA,            // 640x480
-        FRAMESIZE_SVGA,           // 800x600
-        FRAMESIZE_XGA,            // 1024x768
-        FRAMESIZE_HD,             // 1280x720
-        FRAMESIZE_SXGA,           // 1280x1024
-        FRAMESIZE_UXGA,           // 1600x1200
-        config.jpeg_quality = 32; /*It could be anything between 0 and 63.The smaller the number, the higher the quality*/
+    // FRAMESIZE_96X96,              // 96x96
+    // FRAMESIZE_QQVGA,          // 160x120
+    // FRAMESIZE_QCIF,           // 176x144
+    // FRAMESIZE_HQVGA,          // 240x176
+    // FRAMESIZE_240X240,        // 240x240
+    // FRAMESIZE_QVGA,           // 320x240
+    // FRAMESIZE_CIF,            // 400x296
+    // FRAMESIZE_HVGA,           // 480x320
+    // FRAMESIZE_VGA,            // 640x480
+    // FRAMESIZE_SVGA,           // 800x600
+    // FRAMESIZE_XGA,            // 1024x768
+    // FRAMESIZE_HD,             // 1280x720
+    // FRAMESIZE_SXGA,           // 1280x1024
+    // FRAMESIZE_UXGA,           // 1600x1200
+    config.jpeg_quality = 32; /*It could be anything between 0 and 63.The smaller the number, the higher the quality*/
     config.fb_count = 1;
     Serial.println("FRAMESIZE_HVGA");
   }
@@ -144,6 +120,5 @@ void setup()
 
 void loop()
 {
-
   // put your main code here, to run repeatedly:
 }
